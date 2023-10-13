469ebc3313e8217fe643e53e91e0b6c2ab0343b5
alex <brionesalex27@gmail.com>
2023-10-12 18:44:37 -0700

    randomly selected embedded track

--- ai-tunes-react/src/App.js
+++ ai-tunes-react/src/App.js
@@ -5,14 +5,27 @@ import headphones from './headphones.png';
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [showTrack, setShowTrack] = useState(false);
+  const [selectedTrack, setSelectedTrack] = useState('');
+
+  const tracks = [
+    "https://open.spotify.com/track/0CTwVocVcXONwAbRxk4r24?si=7f1e35b574704251",
+    "https://open.spotify.com/track/3pEuO9J2MTEmec8kUfYYvl?si=8287fe8a19ed492f",
+    "https://open.spotify.com/track/65gFUBEh59KEBCznPKQnbG?si=7eb75dded8994596",
+    "https://open.spotify.com/track/5bFuHlXKw66Uu2cHKn5bf8?si=5a23fba2295a401a",
+    "https://open.spotify.com/track/7dQeS4ErW1iWvnKdrtKwKr?si=b1c3d7df1b6d489e",
+    "https://open.spotify.com/track/11LmqTE2naFULdEP94AUBa?si=06d30cd919774c48",
+    "https://open.spotify.com/track/1G391cbiT3v3Cywg8T7DM1?si=fb28b31071db44fc",
+    "https://open.spotify.com/track/5Z3trOR982mBrJcPfrLUsq?si=52963b4c04164a7e"
+  ];
 
   const handleSubmit = () => {
     console.log(inputValue);
-    
-    // Show the embedded track
+
+    const randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
+    setSelectedTrack(randomTrack);
+
     setShowTrack(true);
-    
-    // Clearing the input box after submission
+
     setInputValue('');
   };
 
@@ -30,12 +43,13 @@ function App() {
       <button onClick={handleSubmit} className="submit-button">Submit</button>
       {showTrack && (
         <iframe 
+          title="Spotify Embedded Track"
           style={{borderRadius: '12px', marginTop: '20px'}} 
-          src="https://open.spotify.com/embed/track/4Uiw0Sl9yskBaC6P4DcdVD?utm_source=generator" 
+          src={selectedTrack.replace("open.spotify.com/track", "open.spotify.com/embed/track")} 
           width="700px" 
           height="352" 
-          frameBorder="1" 
-          allowfullscreen="1" 
+          frameBorder="0" 
+          allowFullScreen 
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
           loading="lazy">
         </iframe>
