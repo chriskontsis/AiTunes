7b2e9f4ba552a3873daea9bfabe4d469469630d3
alex <brionesalex27@gmail.com>
2023-10-12 18:15:12 -0700

    commit changes to app.js

--- ai-tunes-react/src/App.js
+++ ai-tunes-react/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import headphones from './headphones.png';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = () => {
+    // You can add any logic here to handle the input value if needed
+    console.log(inputValue);
+    
+    // Clearing the input box after submission
+    setInputValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ai-Tunes
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div className="container">
+      <img src={headphones} alt="Headphones Icon" className="headphones-icon" />
+      <div className="title">AiTunes</div>
+      <input 
+        type="text" 
+        className="input-box"
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+        placeholder="Give us an idea"
+      />
+      <button onClick={handleSubmit} className="submit-button">Submit</button>
     </div>
   );
 }
