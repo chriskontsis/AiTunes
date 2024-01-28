a06eee3731bd7c799e7848fd1a159f335e8d94b0
chriskontsis <cak0927@gmail.com>
2024-01-28 07:34:32 -0700

    initiaitng spotipy authenticator class

--- Reccomender/SpotifyAPIWrapper.py
+++ Reccomender/SpotifyAPIWrapper.py
@@ -10,17 +10,21 @@ GLOBAL_CLIENT_SECRET = '8ddd32222f80488a88cc7819cbcb2fa1'
 
 spotify = None
 
-class SpotifyAPIWrapper:
-    
-    def __init__(self):
-        self.CLIENT_ID = GLOBAL_CLIENT_ID
-        self.CLIENT_SECRET = GLOBAL_CLIENT_SECRET
-        self.spotify = self.doAuth()
 
+class SpotifyAPIWrapper:
+    def __init__(self):
+        authenticator = Authenticator(GLOBAL_CLIENT_ID, GLOBAL_CLIENT_SECRET)
+        self.spotify = authenticator.doAuth()
+        
+
+
+
+class Authenticator:
+    def __init__(self, client_id, client_secret):
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+    
     def doAuth(self):
         credentialManager = SpotifyClientCredentials(client_id=self.CLIENT_ID, client_secret=self.CLIENT_SECRET)
         spotify = spotipy.Spotify(client_credentials_manager=credentialManager)
         return spotify
-
-
-    
