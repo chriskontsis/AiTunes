7847acbf4d5e5e24f37b7a149c3763883ff6fef4
chriskontsis <cak0927@gmail.com>
2024-03-29 16:27:36 -0700

    spider / radar plot seems to be working

--- Reccomender/Visualizer.py
+++ Reccomender/Visualizer.py
@@ -1,4 +1,4 @@
-import math
+from math import pi
 import numpy as np
 import pandas as pd
 import altair as alt
@@ -9,25 +9,36 @@ class Visualizer:
     def __init__(self):
         pass
     
-    def makeRadar(self, df):
+    def makeRadar(self, df, title):
         avgMoods = df.drop(['name'], axis=1).mean().rename_axis('name').reset_index()
-        print(avgMoods)
-        # temp = df.drop(["name"], axis=1)
-        # normalized_df = temp.rank(pct = True)
-        # categories = list(normalized_df)
+        categories = list(avgMoods.iloc[:,0].tolist())
+        N = len(categories)
 
-        # print(categories)
-        # N = len(categories)
+        normalized = avgMoods.iloc[:,1].rank(pct=True)
+        values = normalized.tolist()
+        values += values[:1]
 
+        print(values)
+        print(categories)
+        
         #ax.set_title(str(df['name'].iloc[x*subs+y][:17] + "..."), fontsize=8)
 
+        angles = [n / float(N) * 2 * pi for n in range(N)]
+        angles += angles[:1]
+
+        ax = plt.subplot(111, polar=True)
+        plt.xticks(angles[:-1], categories, color='Black', size=9)
+        ax.set_title(title, fontsize=16)
+        ax.plot(angles, values, linewidth=1, linestyle='solid') 
+        ax.fill(angles, values, 'b', alpha=0.1)
+        plt.show()
 
 
 
 def main():
     df = pd.read_csv("./Data/feature-data/calm_features.csv")
     viz = Visualizer()
-    viz.makeRadar(df)
+    viz.makeRadar(df, "Calm")
     #print(df)
 
 if __name__ == "__main__":
